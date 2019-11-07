7199efeb73fc2303fc8dcfc0963939ea2fd741a3
Daniel Glinka <kaitsh@d-git.de>
2019-11-07 00:21:50 +0000

    Do not flip (causing error in TGAimage lib)

--- tinyrenderer/src/lib.rs
+++ tinyrenderer/src/lib.rs
@@ -4,7 +4,6 @@ pub fn run() {
     let red = TGAColor::rgb(255, 0, 0);
     let mut image = TGAImage::new(100, 100, 24);
     image.set(52, 41, &red);
-    image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
     image.write_tga_file("output.tga", false);
 }
 
